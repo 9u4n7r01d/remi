c93f58cad2114b5215c53deebbc2d8fc3aac79c0
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-12-14 02:41:26 +0700

    Formatting

--- remi/__init__.py
+++ remi/__init__.py
@@ -1,15 +1,16 @@
-import logging
-from rich.logging import RichHandler
-
-
 __name__ = "operator"
 __version__ = "0.1.0"
+
+import logging
+from rich.logging import RichHandler
 
 # Set up logging
 logging.basicConfig(
     level="INFO",
     format="%(message)s",
-    handlers=[RichHandler(rich_tracebacks=True, show_time=True, log_time_format="%y/%m/%d %H:%M:%S")],
+    handlers=[
+        RichHandler(rich_tracebacks=True, show_time=True, log_time_format="%y/%m/%d %H:%M:%S")
+    ],
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
