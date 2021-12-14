a8c215696dfd88a0cf42f43c56b6707512db1202
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-12-14 16:05:48 +0700

    Add argument for the event the bot's listening
    Oops

--- remi/core/bot.py
+++ remi/core/bot.py
@@ -10,15 +10,15 @@ bot = lightbulb.BotApp(token=Config.token, prefix=Config.prefix)
 # Set up some listener for events. In the future we might do some fancy async or DB that needs
 # graceful construction and deconstruction, so it's better to have some scaffold in place beforehand
 @bot.listen(hikari.StartingEvent)
-async def on_starting() -> None:
+async def on_starting(_) -> None:
     pass
 
 
 @bot.listen(hikari.StoppingEvent)
-async def on_stopping() -> None:
+async def on_stopping(_) -> None:
     pass
 
 
 @bot.listen(hikari.StartedEvent)
-async def on_started() -> None:
+async def on_started(_) -> None:
     pass
