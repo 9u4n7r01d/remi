203d8db4d1fdaa0d8f49bfe09f90bc605816940b
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-12-22 01:30:32 +0700

    Migrate logging to `loguru`
    Closes #3.
    
    This is an incredibly hacky implementation of CLI-customizable logging
    from the sheer amount of trickery involved. And as we know it, hacky
    is hard-to-maintain code. I might rewrite this in the future. but for
    now, this is good enough.

--- remi/__init__.py
+++ remi/__init__.py
@@ -1,19 +1,75 @@
 __name__ = "remi"
 __version__ = "0.1.0"
 
-import logging
+import logging as _logging
+import sys as _sys
 
+import click as _click
 from dotenv import load_dotenv
-from rich.logging import RichHandler
+from loguru import logger as _logger
 
 load_dotenv()
 
-# Set up logging
-logging.basicConfig(
-    format="%(message)s",
-    handlers=[
-        RichHandler(rich_tracebacks=True, show_time=True, log_time_format="%y/%m/%d %H:%M:%S")
-    ],
-)
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+# Logging interceptor for directing logging to loguru.logger
+class _InterceptHandler(_logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = _logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = _logging.currentframe(), 2
+        while frame.f_code.co_filename == _logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        _logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+# Voodoo magic from StackOverflow to make click work from __init__.py
+#
+# Somehow, if chucked inside __init__, the usual click syntax fails if defined and called here.
+# Debugging shows that click exits due to standalone mode being True. So let's do some magic to get
+# the context, and manual labor to exit if `--help`
+@_click.command()
+@_click.option("-v", "--verbose", help="Increase verbosity (can be stacked).", count=True)
+@_click.pass_context
+def get_click_context(ctx, *args, **kwargs):
+    return ctx
+
+
+_ctx = get_click_context(standalone_mode=False)
+
+if not _ctx:  # If we're calling with --help, get_context() will return 0
+    _sys.exit(0)
+
+else:
+    # Mapping for logging level
+    match _ctx.params["verbose"]:
+        case 0:
+            _logging_level = 20  # INFO
+            _loguru_level_padding = 8
+        case 1:
+            _logging_level = 10  # DEBUG
+            _loguru_level_padding = 8
+        case 2:
+            _logging_level = 5  # TRACE_HIKARI
+            _loguru_level_padding = 12
+        case _:
+            _logging_level = 0  # NOTSET
+            _loguru_level_padding = 12
+
+    # Remove the default handler and replace it with our customizable one, and start logging
+    _logger.remove()
+
+    _log_format = (
+        "<g>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | "
+        f"<lvl>{{level: <{_loguru_level_padding}}}</> | "
+        "<c>{name}</>:<c>{function}</>:<c>{line}</> - <lvl>{message}</>"
+    )
+
+    _logger.add(_sys.stderr, format=_log_format, level=_logging_level)
+    _logging.basicConfig(handlers=[_InterceptHandler()], level=_logging_level)
--- remi/__main__.py
+++ remi/__main__.py
@@ -1,33 +1,11 @@
-import logging
 import os
-
-import click
 
 from remi.core import bot
 
+if os.sys != "nt":
+    import uvloop
 
-@click.command()
-@click.option("-v", "--verbose", help="Increase verbosity (can be stacked).", count=True)
-def main(verbose: int) -> None:
-    # Mapping for logging level
-    match verbose:
-        case 0:
-            logging_level = "INFO"
-        case 1:
-            logging_level = "DEBUG"
-        case _:
-            logging_level = "NOTSET"
-
-    # Set root logger's logging level
-    logging.getLogger().setLevel(logging_level)
-
-    # Use uvloop if we're not on Windows
-    if os.name != "nt":
-        import uvloop
-
-        uvloop.install()
-
-    bot.run()
+    uvloop.install()
 
 
-main()
+bot.run()
