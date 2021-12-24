9u4n7r01d/remi

commit e0732a8b880d0a2e6ff86addabc9be6c63d88d7c

Wrap `bot.run()` in `main()` for Poetry script
This was removed due to an oversight in commit 203d8db4d1, causing
`remi.__main__() is not callable` to be thrown.

remi/__main__.py
@@ -8,4 +8,8 @@ if os.sys != "nt":
     uvloop.install()
 
 
-bot.run()
+def main():
+    bot.run()
+
+
+main()

pyproject.toml
@@ -25,7 +25,7 @@ pytest-repeat = "^0.9.1"
 isort = "^5.10.1"
 
 [tool.poetry.scripts]
-remi = 'remi:__main__'
+remi = 'remi:__main__.main'
 
 [build-system]
 requires = ["poetry-core>=1.0.0"]

commit 0cfbddc4ca8c11646ac6f78103ad3bf682066863

Ensure `main()` won't run twice via Poetry
Oops. This bug was introduced in commit e0732a8b88 due to oversight.

remi/__main__.py
@@ -12,4 +12,5 @@ def main():
     bot.run()
 
 
-main()
+if __name__ == "__main__":
+    main()