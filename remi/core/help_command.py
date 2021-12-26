85caa70559918266ca2e056b306a020f771ae422
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-12-26 14:28:43 +0700

    Refactor `[p]help`
    The original implementation was nearly a one-to-one copy of
    `lightbulb.DefaultHelpCommand` including things like `defaultdict` and
    dictionary sorting which makes it overly complicated. By eliminating
    unnecessary `dict` preparation, `[p]help` should be more maintainable.

--- remi/core/help_command.py
+++ remi/core/help_command.py
@@ -1,6 +1,3 @@
-import typing
-from collections import defaultdict
-
 import hikari
 import lightbulb
 from lightbulb import commands, context, plugins
@@ -12,10 +9,6 @@ from remi.util.embed import create_embed_from_dict
 
 
 class HelpCommand(lightbulb.BaseHelpCommand):
-    @staticmethod
-    def _sort_dict(target: dict):
-        return {k: v for k, v in sorted(target.items(), key=lambda ele: ele[0])}
-
     @staticmethod
     async def _gather_commands(plugin: plugins.Plugin, ctx: context.Context):
         """Recursively gather all unique commands of a cog and description"""
@@ -45,19 +38,14 @@ class HelpCommand(lightbulb.BaseHelpCommand):
 
     async def send_bot_help(self, ctx: context.Context) -> None:
         """Default help message when `[p]help` is called without arguments"""
-        # Gather extensions' information
-        plugin_dict = defaultdict(dict)
-        for name, plugin in self.bot.plugins.items():
-            plugin_dict[name]["description"] = plugin.description
-            plugin_dict[name]["commands"] = await self._gather_commands(plugin, ctx)
-
         # Prepare the embed
         help_embed = EmbedPaginator(max_lines=30, max_chars=1024)
         help_embed.set_embed_factory(self._build_bot_help_embed)
 
-        for name in plugin_dict:
-            help_embed.add_line(f"__**{name}** - *{plugin_dict[name]['description']}*__")
-            [help_embed.add_line(line) for line in plugin_dict[name]["commands"]]
+        # Build the embed
+        for name, plugin in self.bot.plugins.items():
+            help_embed.add_line(f"__**{name}** - *{plugin.description}*__")
+            [help_embed.add_line(line) for line in await self._gather_commands(plugin, ctx)]
             help_embed.add_line("")
 
         navigator = ButtonNavigator(help_embed.build_pages())
