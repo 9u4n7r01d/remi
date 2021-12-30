0acf69a7c495abb1c7b6876c6049713b16953d78
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2021-12-31 02:29:06 +0700

    Handle exception raised in parsing `OWNER_IDS`

--- remi/core/config.py
+++ remi/core/config.py
@@ -1,8 +1,17 @@
+import logging
 import os
 from typing import Final
+
+
+def parse_owner_ids():
+    try:
+        return tuple(int(i) for i in os.getenv("OWNER_IDS").split(","))
+    except (AttributeError, ValueError):
+        logging.warning("Could not parse environment variable OWNER_IDS, using default owner(s)")
+        return ()
 
 
 class Config:
     token: Final[str] = os.getenv("TOKEN")
     prefix: Final[str] = os.getenv("BOT_PREFIX")
-    owner_ids: Final[tuple[int]] = tuple(int(i) for i in os.getenv("OWNER_IDS").split(","))
+    owner_ids: Final[tuple[int]] = parse_owner_ids()
