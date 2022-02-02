adc5cae6c3621983a05652f1333ef045df71bebf
PythonTryHard <31789326+PythonTryHard@users.noreply.github.com>
2022-02-02 16:03:32 +0700

    Refactor to use proper typed dict

--- remi/command/core/about/about.py
+++ remi/command/core/about/about.py
@@ -7,6 +7,7 @@ from lightbulb import commands, context
 
 from remi.core.constant import Global
 from remi.util.embed import create_embed_from_dict
+from remi.util.typing import EmbedDict, EmbedField
 
 about = lightbulb.Plugin("About", description="Information about Remi.")
 
@@ -29,21 +30,19 @@ async def core_about(ctx: context.Context):
     red_repo = "https://github.com/Cog-Creators/Red-DiscordBot"
 
     info_embed = create_embed_from_dict(
-        {
-            "color": 0xF9DC5C,
-            "fields": [
-                {"name": "Instance's owner", "value": owner, "inline": True},
-                {
-                    "name": "Built on",
-                    "value": "\n".join(
-                        [f"\N{BULLET} {i.__name__} v{i.__version__}" for i in (hikari, lightbulb, loguru)]
-                    ),
-                    "inline": True,
-                },
-                {"name": "Python version", "value": f"{sys.version}"},
-                {
-                    "name": "About the bot",
-                    "value": (
+        EmbedDict(
+            color=0xF9DC5C,
+            fields=[
+                EmbedField(name="Instance's owner", value=owner, inline=True),
+                EmbedField(
+                    name="Built on",
+                    value="\n".join([f"\N{BULLET} {i.__name__} v{i.__version__}" for i in (hikari, lightbulb, loguru)]),
+                    inline=True,
+                ),
+                EmbedField(name="Python version", value=f"{sys.version}"),
+                EmbedField(
+                    name="About the bot",
+                    value=(
                         f"This bot is an instance of [`remi`]({remi_repo}), an open-source bot"
                         f"made by [PythonTryHard]({author}), taking inspiration from "
                         f"[`Red-DiscordBot`]({red_repo}), without which, I'd still be afraid to "
@@ -52,10 +51,10 @@ async def core_about(ctx: context.Context):
                         " issue on GitHub and I'll try to respond as quickly as possible.\n"
                         "PythonTryHard"
                     ),
-                },
+                ),
             ],
-            "footer": {"text": f"Made with <3"},
-            "thumbnail": ctx.bot.get_me().avatar_url,
-        }
+            footer={"text": f"Made with <3"},
+            thumbnail=ctx.bot.get_me().avatar_url,
+        )
     )
     await ctx.respond(embed=info_embed)
